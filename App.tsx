13e93f6665eb3006e3cf65ec3a1bb25c21b13fd1
anshuman-coder <anshurockingboy23@gmail.com>
2024-07-13 20:13:16 +0530

    boilerplate setup

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { styled } from 'nativewind'
+
+const ViewStyled = styled(View)
+const TextStyled = styled(Text)
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <ViewStyled className="flex-1 items-center justify-center bg-white">
+      <TextStyled className='italic'>Open up App.tsx to start working on your app!</TextStyled>
       <StatusBar style="auto" />
-    </View>
+    </ViewStyled>
   );
 }
 
